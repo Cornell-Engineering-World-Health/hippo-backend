d885f30c298305a74e45ded3f2333aef05bd3115
Frank Chan <fc249@cornell.edu>
2017-02-16 19:13:38 -0500

    Add CORS filter

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
