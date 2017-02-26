7e5594fd3dcf7315f09ca3f40ffad976bce3b60b
Frank Chan <fc249@cornell.edu>
2017-02-26 17:13:31 -0500

    comment out currently unused vars

--- test/services/testVideos.js
+++ test/services/testVideos.js
@@ -1,8 +1,8 @@
 /* eslint-env mocha */
 require('dotenv').config()
 var sinon = require('sinon')
-var chai = require('chai')
-//var expect = chai.expect
+// var chai = require('chai')
+// var expect = chai.expect
 var videoService = require('../../services/videos')
 
 describe('Video Service', function () {
