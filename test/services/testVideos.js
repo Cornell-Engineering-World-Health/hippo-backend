e71f5c6fee57831f101be0982565ed2ec0021392
Sonia Appasamy <sma246@cornell.edu>
2017-03-02 18:43:06 -0500

    added testing of the random name generation

--- test/services/testVideos.js
+++ test/services/testVideos.js
@@ -1,14 +1,19 @@
 /* eslint-env mocha */
 require('dotenv').config()
+require('sinon-mongoose')
 var sinon = require('sinon')
 // var chai = require('chai')
 // var expect = chai.expect
 var videoService = require('../../services/videos')
+var Videocall = require('../../models/videocall')
 
 describe('Video Service', function () {
   describe('generateChatName', function () {
     it('should return a unique chat name', sinon.test(function () {
-      //
+      sinon.mock(Videocall)
+        .expects('findOne')
+        .chain('exec')
+        .yields(null, null)
       videoService.generateChatName()
     }))
   })
